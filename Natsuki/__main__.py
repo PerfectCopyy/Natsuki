99c674fb07849edc36d5ebc5ebd2ef52c4a855b4
Vivooo <91451593+PerfectCopyy@users.noreply.github.com>
2021-09-27 12:37:09 +0530

    Update __main__.py

--- Natsuki/__main__.py
+++ Natsuki/__main__.py
@@ -63,8 +63,9 @@ from Natsuki.modules.helper_funcs.misc import paginate_modules
 from Natsuki.modules.helper_funcs.readable_time import get_readable_time
 
 PM_START_TEXT = """
-👋 Hey There, My Name is Shimizu Kiyoko"https://telegra.ph/file/1277852bc55f4af75d1d4.jpg". 
-I'm a Powerfull Group Manager Bot With Cool Modules. feel free to add me to your groups!
+👋 Hey There!` [👩‍💼](https://telegra.ph/file/1277852bc55f4af75d1d4.jpg) `My name is` *Shimizu Kiyoko*
+`I am an Anime Themed group management bot.
+Made by ItsVivooo for Your Telegram Group , I specialize in managing anime and similar themed groups.
 """
 
 buttons = [
